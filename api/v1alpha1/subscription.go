6b63f1e2582e14ab8dc8eed03282067798272312
Abirdcfly <fp544037857@gmail.com>
2023-08-21 09:50:24 +0800

    feat: add Schedule to SubscriptionSpec
    Signed-off-by: Abirdcfly <fp544037857@gmail.com>

--- api/v1alpha1/subscription.go
+++ api/v1alpha1/subscription.go
@@ -17,10 +17,17 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"fmt"
 	"strings"
+	"time"
 
+	"github.com/robfig/cron/v3"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	SubscriptionNameLabel = Group + "/subscription-name"
 )
 
 // IsAuto returns true if InstallMethod is Auto, case-insensitivity.
@@ -91,4 +98,77 @@ func SubscriptionReconcileError(ct ConditionType, err error) Condition {
 		Reason:             SubscriptionReasonReconcileError,
 		Message:            err.Error(),
 	}
+}
+
+// MostRecentScheduleTime returns:
+//   - CronJob's creation time,
+//   - the most recent time a Job should be created or nil, if that's after now,
+//   - boolean indicating an excessive number of missed schedules,
+//   - error in an edge case where the schedule specification is grammatically correct,
+//     but logically doesn't make sense (31st day for months with only 30 days, for example).
+//
+// inspire by https://github.com/kubernetes/kubernetes/blob/2fe38f93e53201b0c9e58aa6e3d37b8a61d2ca23/pkg/controller/cronjob/utils.go#L81
+func (r *Subscription) MostRecentScheduleTime(now time.Time, schedule cron.Schedule) (time.Time, *time.Time, bool, error) {
+	earliestTime := r.ObjectMeta.CreationTimestamp.Time
+	t1 := schedule.Next(earliestTime)
+	t2 := schedule.Next(t1)
+	if now.Before(t1) {
+		// now  t1
+		return earliestTime, nil, false, nil
+	}
+	if now.Before(t2) {
+		// t1 now t2
+		return earliestTime, &t1, false, nil
+	}
+
+	// It is possible for cron.ParseStandard("59 23 31 2 *") to return an invalid schedule
+	// minute - 59, hour - 23, dom - 31, month - 2, and dow is optional, clearly 31 is invalid
+	// In this case the timeBetweenTwoSchedules will be 0, and we error out the invalid schedule
+	timeBetweenTwoSchedules := int64(t2.Sub(t1).Round(time.Second).Seconds())
+	if timeBetweenTwoSchedules < 1 {
+		return earliestTime, nil, false, fmt.Errorf("time difference between two schedules is less than 1 second")
+	}
+	// this logic used for calculating number of missed schedules does a rough
+	// approximation, by calculating a diff between two schedules (t1 and t2),
+	// and counting how many of these will fit in between last schedule and now
+	timeElapsed := int64(now.Sub(t1).Seconds())
+	numberOfMissedSchedules := (timeElapsed / timeBetweenTwoSchedules) + 1
+
+	var mostRecentTime time.Time
+	// to get the most recent time accurate for regular schedules and the ones
+	// specified with @every form, we first need to calculate the potential earliest
+	// time by multiplying the initial number of missed schedules by its interval,
+	// this is critical to ensure @every starts at the correct time, this explains
+	// the numberOfMissedSchedules-1, the additional -1 serves there to go back
+	// in time one more time unit, and let the cron library calculate a proper
+	// schedule, for case where the schedule is not consistent, for example
+	// something like  30 6-16/4 * * 1-5
+	potentialEarliest := t1.Add(time.Duration((numberOfMissedSchedules-1-1)*timeBetweenTwoSchedules) * time.Second)
+	for t := schedule.Next(potentialEarliest); !t.After(now); t = schedule.Next(t) {
+		mostRecentTime = t
+	}
+
+	// An object might miss several starts. For example, if
+	// controller gets wedged on friday at 5:01pm when everyone has
+	// gone home, and someone comes in on tuesday AM and discovers
+	// the problem and restarts the controller, then all the hourly
+	// jobs, more than 80 of them for one hourly cronJob, should
+	// all start running with no further intervention (if the cronJob
+	// allows concurrency and late starts).
+	//
+	// However, if there is a bug somewhere, or incorrect clock
+	// on controller's server or apiservers (for setting creationTimestamp)
+	// then there could be so many missed start times (it could be off
+	// by decades or more), that it would eat up all the CPU and memory
+	// of this controller. In that case, we want to not try to list
+	// all the missed start times.
+	//
+	// I've somewhat arbitrarily picked 100, as more than 80,
+	// but less than "lots".
+	tooManyMissed := numberOfMissedSchedules > 100
+
+	if mostRecentTime.IsZero() {
+		return earliestTime, nil, tooManyMissed, nil
+	}
+	return earliestTime, &mostRecentTime, tooManyMissed, nil
 }
--- api/v1alpha1/subscription_test.go
+++ api/v1alpha1/subscription_test.go
@@ -18,9 +18,13 @@ package v1alpha1
 
 import (
 	"errors"
+	"reflect"
 	"testing"
+	"time"
 
+	"github.com/robfig/cron/v3"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // TestIsAuto for InstallMethod.IsAuto
@@ -75,4 +79,195 @@ func TestSubscriptionReconcileError(t *testing.T) {
 	if r := SubscriptionReconcileError(cond.Type, err); r.Type != expCond.Type || r.Status != expCond.Status || r.Reason != expCond.Reason || r.Message != expCond.Message {
 		t.Fatalf("Test Failed, expect %v get %v", expCond, r)
 	}
+}
+
+// inspire by https://github.com/kubernetes/kubernetes/blob/2fe38f93e53201b0c9e58aa6e3d37b8a61d2ca23/pkg/controller/cronjob/utils_test.go#L353
+func TestMostRecentScheduleTime(t *testing.T) {
+	metav1TopOfTheHour := metav1.NewTime(*topOfTheHour())
+
+	tests := []struct {
+		name                  string
+		sub                   *Subscription
+		includeSDS            bool
+		now                   time.Time
+		expectedEarliestTime  time.Time
+		expectedRecentTime    *time.Time
+		expectedTooManyMissed bool
+		wantErr               bool
+	}{
+		{
+			name: "now before next schedule",
+			sub: &Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					CreationTimestamp: metav1TopOfTheHour,
+				},
+				Spec: SubscriptionSpec{
+					Schedule: "0 * * * *",
+				},
+			},
+			now:                  topOfTheHour().Add(30 * time.Second),
+			expectedRecentTime:   nil,
+			expectedEarliestTime: *topOfTheHour(),
+		},
+		{
+			name: "now just after next schedule",
+			sub: &Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					CreationTimestamp: metav1TopOfTheHour,
+				},
+				Spec: SubscriptionSpec{
+					Schedule: "0 * * * *",
+				},
+			},
+			now:                  topOfTheHour().Add(61 * time.Minute),
+			expectedRecentTime:   deltaTimeAfterTopOfTheHour(60 * time.Minute),
+			expectedEarliestTime: *topOfTheHour(),
+		},
+		{
+			name: "missed 5 schedules",
+			sub: &Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					CreationTimestamp: metav1.NewTime(*deltaTimeAfterTopOfTheHour(10 * time.Second)),
+				},
+				Spec: SubscriptionSpec{
+					Schedule: "0 * * * *",
+				},
+			},
+			now:                  *deltaTimeAfterTopOfTheHour(301 * time.Minute),
+			expectedRecentTime:   deltaTimeAfterTopOfTheHour(300 * time.Minute),
+			expectedEarliestTime: *deltaTimeAfterTopOfTheHour(10 * time.Second),
+		},
+		{
+			name: "complex schedule",
+			sub: &Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					CreationTimestamp: metav1TopOfTheHour,
+				},
+				Spec: SubscriptionSpec{
+					Schedule: "30 6-16/4 * * 1-5",
+				},
+			},
+			now:                  *deltaTimeAfterTopOfTheHour(24*time.Hour + 31*time.Minute),
+			expectedRecentTime:   deltaTimeAfterTopOfTheHour(24*time.Hour + 30*time.Minute),
+			expectedEarliestTime: *topOfTheHour(),
+		},
+		{
+			name: "another complex schedule",
+			sub: &Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					CreationTimestamp: metav1TopOfTheHour,
+				},
+				Spec: SubscriptionSpec{
+					Schedule: "30 10,11,12 * * 1-5",
+				},
+			},
+			now:                  *deltaTimeAfterTopOfTheHour(30*time.Hour + 30*time.Minute),
+			expectedRecentTime:   nil,
+			expectedEarliestTime: *topOfTheHour(),
+		},
+		{
+			name: "complex schedule with longer diff between executions",
+			sub: &Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					CreationTimestamp: metav1TopOfTheHour,
+				},
+				Spec: SubscriptionSpec{
+					Schedule: "30 6-16/4 * * 1-5",
+				},
+			},
+			now:                  *deltaTimeAfterTopOfTheHour(96*time.Hour + 31*time.Minute),
+			expectedRecentTime:   deltaTimeAfterTopOfTheHour(96*time.Hour + 30*time.Minute),
+			expectedEarliestTime: *topOfTheHour(),
+		},
+		{
+			name: "complex schedule with shorter diff between executions",
+			sub: &Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					CreationTimestamp: metav1TopOfTheHour,
+				},
+				Spec: SubscriptionSpec{
+					Schedule: "30 6-16/4 * * 1-5",
+				},
+			},
+			now:                  *deltaTimeAfterTopOfTheHour(24*time.Hour + 31*time.Minute),
+			expectedRecentTime:   deltaTimeAfterTopOfTheHour(24*time.Hour + 30*time.Minute),
+			expectedEarliestTime: *topOfTheHour(),
+		},
+		{
+			name: "@every schedule",
+			sub: &Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					CreationTimestamp: metav1.NewTime(*deltaTimeAfterTopOfTheHour(-59 * time.Minute)),
+				},
+				Spec: SubscriptionSpec{
+					Schedule: "@every 1h",
+				},
+			},
+			now:                   *deltaTimeAfterTopOfTheHour(7 * 24 * time.Hour),
+			expectedRecentTime:    deltaTimeAfterTopOfTheHour((6 * 24 * time.Hour) + 23*time.Hour + 1*time.Minute),
+			expectedEarliestTime:  *deltaTimeAfterTopOfTheHour(-59 * time.Minute),
+			expectedTooManyMissed: true,
+		},
+		{
+			name: "rogue cronjob",
+			sub: &Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					CreationTimestamp: metav1.NewTime(*deltaTimeAfterTopOfTheHour(10 * time.Second)),
+				},
+				Spec: SubscriptionSpec{
+					Schedule: "59 23 31 2 *",
+				},
+			},
+			now:                *deltaTimeAfterTopOfTheHour(1 * time.Hour),
+			expectedRecentTime: nil,
+			wantErr:            true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sched, err := cron.ParseStandard(tt.sub.Spec.Schedule)
+			if err != nil {
+				t.Errorf("error setting up the test, %s", err)
+			}
+			gotEarliestTime, gotRecentTime, gotTooManyMissed, err := tt.sub.MostRecentScheduleTime(tt.now, sched)
+			if tt.wantErr {
+				if err == nil {
+					t.Error("mostRecentScheduleTime() got no error when expected one")
+				}
+				return
+			}
+			if !tt.wantErr && err != nil {
+				t.Error("mostRecentScheduleTime() got error when none expected")
+			}
+			if gotEarliestTime.IsZero() {
+				t.Errorf("earliestTime should never be 0, want %v", tt.expectedEarliestTime)
+			}
+			if !gotEarliestTime.Equal(tt.expectedEarliestTime) {
+				t.Errorf("expectedEarliestTime - got %v, want %v", gotEarliestTime, tt.expectedEarliestTime)
+			}
+			if !reflect.DeepEqual(gotRecentTime, tt.expectedRecentTime) {
+				t.Errorf("expectedRecentTime - got %v, want %v", gotRecentTime, tt.expectedRecentTime)
+			}
+			if gotTooManyMissed != tt.expectedTooManyMissed {
+				t.Errorf("expectedNumberOfMisses - got %v, want %v", gotTooManyMissed, tt.expectedTooManyMissed)
+			}
+		})
+	}
+}
+
+func topOfTheHour() *time.Time {
+	T1, err := time.Parse(time.RFC3339, "2016-05-19T10:00:00Z")
+	if err != nil {
+		panic("test setup error")
+	}
+	return &T1
+}
+
+func deltaTimeAfterTopOfTheHour(duration time.Duration) *time.Time {
+	T1, err := time.Parse(time.RFC3339, "2016-05-19T10:00:00Z")
+	if err != nil {
+		panic("test setup error")
+	}
+	t := T1.Add(duration)
+	return &t
 }
